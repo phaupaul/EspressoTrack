79d5315be5dca76db85d1e2142e199c405e11e84
phaupaul <paul.phau@gmail.com>
2026-02-05 09:47:20 -0800

    Fix Vercel deployment configuration

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { registerRoutes } from "../dist/routes.js";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+registerRoutes(app);
+
+export default app;
